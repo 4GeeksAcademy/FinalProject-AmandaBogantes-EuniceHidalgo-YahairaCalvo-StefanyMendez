e80ebaced29d1c51fde3f439953c91039ccc42ac
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2023-09-07 21:29:21 +0000

    adding last changes to backToTopBtn and backToTopBtn.css

--- src/front/js/component/backToTopBtn.js
+++ src/front/js/component/backToTopBtn.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import "../../styles/backToTopBtn.css"
 
 
 
@@ -7,29 +8,34 @@ const BackToTopBtn = () => {
 
     const [backToTopBtn, setbackToTopBtn] = useState(false);
 
+    const handleVisibility=() => {
+        if (window.scrollY > 100) {
+            setbackToTopBtn(true)
+        } else {
+            setbackToTopBtn(false)
+        }
+    }
+
     useEffect(() => {
-        window.addEventListener("scroll", () => {
-            if (window.scrollY > 100) {
-                setbackToTopBtn(true)
-            } else {
-                setbackToTopBtn(false)
-            }
-        })
+        window.addEventListener("scroll", handleVisibility )
+        return ()=> window.removeEventListener("scroll", handleVisibility)
     }, [])
 
     const scrollUp = () => {
-        window.scrollTo
-        top: 0;
-        behavior: "smooth";
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+        
     }
     return (
-        <div className=''>
+        <div className='go-top-btn'>
 
             {backToTopBtn && (
 
-                <button onClick={scrollUp}  >
+                <div onClick={scrollUp}  >
                     <i className="fa-solid fa-angles-up"></i>
-                </button>
+                </div>
             )
             }
 
