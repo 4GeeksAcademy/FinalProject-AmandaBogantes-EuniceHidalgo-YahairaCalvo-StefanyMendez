d27426c03c01d1a266bcf5e1ef622c76401ccbe3
abarrelier <128666422+abarrelier@users.noreply.github.com>
2023-09-10 07:02:17 +0000

    [fixNode] --fix css

--- src/front/js/component/contactUsTable.js
+++ src/front/js/component/contactUsTable.js
@@ -3,49 +3,49 @@ import { Context } from "../store/appContext";
 
 
 export const ContactUsTable = () => {
-    const {store, actions} = useContext(Context)
+    const { store, actions } = useContext(Context)
     const [formData, setFormData] = useState({})
-    const handleChange = (e) =>{
-        e.preventDefault() 
+    const handleChange = (e) => {
+        e.preventDefault()
         setFormData({
             ...formData,
             [e.target.name]: e.target.value
         })
     }
     return (
-        <div className="container">
-            <h5 className='title-contactUs md-3'>We Are For You</h5>
-            <p className='text-contactus'>Our team are here to help <i className="fa-solid fa-heart"></i></p>
+        <div className="container container-contactUs">
+            <h5 className='title-contactUs md-3'>We Are Here For You</h5>
+            <p className='text-contactus'>Our team are here to help <i className="fa-solid fa-heart i-contactUs"></i></p>
             <div className="row">
                 <div className="col-md-6">
-                    <form onSubmit={(e)=>{actions.handleSubmit(e,formData)}}>
+                    <form className="form-contactUs" onSubmit={(e) => { actions.handleSubmit(e, formData) }}>
                         <div className="row mb-4">
                             <div className="col">
-                                <label for="Name" className="form-label">Name</label>
-                                <input type="text" className="form-control" id="ID" placeholder="Juan" name="name" onChange={handleChange}/>
+                                <label for="Name" className="form-label form-labelContactUs">Name</label>
+                                <input type="text" className="form-control form-controlContactUs" id="ID" placeholder="Juan" name="name" onChange={handleChange} />
                             </div>
                             <div className="col">
-                                <label for="lastName" className="form-label">Last Name</label>
-                                <input type="text" className="form-control" id="lastName" placeholder="Pérez" name="lastName" onChange={handleChange}/>
+                                <label for="lastName" className="form-label form-labelContactUs">Last Name</label>
+                                <input type="text" className="form-control form-controlContactUs" id="lastName" placeholder="Pérez" name="lastName" onChange={handleChange} />
                             </div>
                         </div>
                         <div className="row mb-4">
                             <div className="col">
-                                <label for="phone" className="form-label">Phone</label>
-                                <input type="tel" className="form-control" id="phone" placeholder="(123) 456-7890" name="phone" onChange={handleChange}/>
+                                <label for="phone" className="form-label form-labelContactUs">Phone</label>
+                                <input type="tel" className="form-control form-controlContactUs" id="phone" placeholder="(123) 456-7890" name="phone" onChange={handleChange} />
                             </div>
                             <div className="col">
-                                <label for="mail" className="form-label">Email</label>
-                                <input type="email" className="form-control" id="mail" placeholder="mail@example.com" name="email" onChange={handleChange} />
+                                <label for="mail" className="form-label form-labelContactUs">Email</label>
+                                <input type="email" className="form-control form-controlContactUs" id="mail" placeholder="mail@example.com" name="email" onChange={handleChange} />
                             </div>
                         </div>
                         <div className="mb-4">
-                            <label for="consult" className="form-label">Topic</label>
-                            <input type="text" className="form-control" id="consult" placeholder="Question" name="question" onChange={handleChange}/>
+                            <label for="consult" className="form-label form-labelContactUs">Topic</label>
+                            <input type="text" className="form-control form-controlContactUs" id="consult" placeholder="Question" name="question" onChange={handleChange} />
                         </div>
                         <div className="mb-4">
-                            <label for="message" className="form-label">Message</label>
-                            <textarea className="form-control" id="message" rows="4" placeholder="Your Message Here" name="message" onChange={handleChange}/>
+                            <label for="message" className="form-label form-labelContactUs">Message</label>
+                            <textarea className="form-control form-controlContactUs" id="message" rows="4" placeholder="Your Message Here" name="message" onChange={handleChange} />
                         </div>
                         <button type="submit" className="btn-contact">Send</button>
                     </form>
@@ -56,19 +56,19 @@ export const ContactUsTable = () => {
             </div>
             <div className="row social-information">
                 <div className="col-md-2">
-                    <div className="icon-contact"><i className="far fa-address-book"></i> San Jose, Costa Rica</div>
+                    <div className="icon-contact"><i className="far fa-address-book i-contactUs"></i> San Jose, Costa Rica</div>
                 </div>
                 <div className="col-md-2">
-                    <div className="icon-contact"><i className="fab fa-whatsapp"></i> (506) 88-88-88-88</div>
+                    <div className="icon-contact"><i className="fab fa-whatsapp i-contactUs"></i> (506) 88-88-88-88</div>
                 </div>
                 <div className="col-md-2">
-                    <div className="icon-contact"><i className="fas fa-phone"></i> 22-22-22-22</div>
+                    <div className="icon-contact"><i className="fas fa-phone i-contactUs"></i> 22-22-22-22</div>
                 </div>
                 <div className="col-md-2">
-                    <div className="icon-contact"><i className="fab fa-facebook"></i> EyASolutions</div>
+                    <div className="icon-contact"><i className="fab fa-facebook i-contactUs"></i> EyASolutions</div>
                 </div>
                 <div className="col-md-2">
-                    <div className="icon-contact"><i className="fab fa-instagram"></i> EyA_Solutions</div>
+                    <div className="icon-contact"><i className="fab fa-instagram i-contactUs"></i> EyA_Solutions</div>
                 </div>
             </div>
         </div>
